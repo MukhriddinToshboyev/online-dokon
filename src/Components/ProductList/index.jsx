fd2a40c91b0af9f0bd0328e7b715bfc76db56c53
Muxriddin Toshboyev <167794484+MukhriddinToshboyev@users.noreply.github.com>
2025-05-21 15:18:15 +0500

    Fix formatting and add missing semicolons in ProductList component

--- src/Components/ProductList/index.jsx
+++ src/Components/ProductList/index.jsx
@@ -1,11 +1,12 @@
-import products from "../../products"
+import products from "../../products";
+import ProductCard from "../ProductCard";
 
-export default function ProductList( ) {
-
-   return (
-        <div className="products">
-             {products.map((product) => (
-                <ProductCard key={product.id} product={product} />
-             ))}
-        </div>
-   )
+export default function ProductList() {
+  return (
+    <div className="products">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
+}
