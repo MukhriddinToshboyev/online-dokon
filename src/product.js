62302f1d0d95630acfe5fdefc182fb6ed3a94470
Muxriddin Toshboyev <167794484+MukhriddinToshboyev@users.noreply.github.com>
2025-05-21 14:54:50 +0500

    online market

---
+++ src/product.js
@@ -0,0 +1,65 @@
+const products = [
+  {
+    id: 1,
+    name: "Telefon",
+    price: 5000000,
+    Image: "./assets/productImage/Samsung A56.webp",
+  },
+  {
+    id: 2,
+    name: "Noutbuk",
+    price: 3500000,
+    Image: "./assets/productImage/Noutbuk.webp",
+  },
+  {
+    id: 3,
+    name: "Televizor",
+    price: 1500000,
+    Image: "./assets/productImage/Televizor.webp",
+  },
+  {
+    id: 4,
+    name: "Smartwatch",
+    price: 300000,
+    Image: "./assets/productImage/SmartWatch.webp",
+  },
+  {
+    id: 5,
+    name: "Mikrofon",
+    price: 100000,
+    Image: "./assets/productImage/Mikrafon.webp",
+  },
+  {
+    id: 6,
+    name: "Kamera",
+    price: 600000,
+    Image: "./assets/productImage/Kamera.webp",
+  },
+  {
+    id: 7,
+    name: "Kondisioner",
+    price: 4000000,
+    Image: "./assets/productImage/Konditsioner.webp",
+  },
+  {
+    id: 8,
+    name: "Mikrovolnovka",
+    price: 800000,
+    Image: "./assets/productImage/Mikrovolnofka.webp",
+  },
+  {
+    id: 9,
+    name: "Plita",
+    price: 1200000,
+    Image: "./assets/productImage/Plita.webp",
+  },
+
+  {
+    id: 10,
+    name: "Printer",
+    price: 1600000,
+    Image: "./assets/productImage/Printir.webp",
+  },
+];
+
+export default products;
